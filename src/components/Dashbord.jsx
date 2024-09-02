0dd4d47b9fc60c1f1596d0aa506e827dd7e97f2f
KhushbuWebapp <khushbu.webappdev@gmail.com>
2024-09-02 15:56:43 +0530

    add detials

--- src/components/Dashbord.jsx
+++ src/components/Dashbord.jsx
@@ -5,6 +5,22 @@ const Dashbord = () => {
   return (
     <Box width={"50%"} height={200}>
       Dashborad page...
+      <hr />
+      <Box component={"p"} textAlign={"justify"}>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda velit
+        nesciunt possimus doloremque ad sed, nostrum alias totam ipsam incidunt
+        itaque impedit architecto laudantium, voluptatem consequuntur fugiat
+        unde repellat, perferendis eius est quibusdam? Facere provident ipsa
+        doloremque explicabo voluptatibus consequatur architecto. Deleniti dicta
+        quis ut nisi, molestias aliquid ratione libero explicabo quos
+        repudiandae aliquam eius qui quibusdam facere culpa nulla dolore aut
+        neque esse. Voluptate rem officiis beatae quod nobis non vero, mollitia
+        magnam modi corrupti eius eum magni totam ratione tempora adipisci quasi
+        impedit voluptatem, exercitationem culpa! Expedita, reiciendis!
+        Architecto neque omnis voluptatibus velit suscipit, dolorum culpa harum
+        eos.
+      </Box>
     </Box>
   );
 };
